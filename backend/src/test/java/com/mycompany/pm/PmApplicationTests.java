70a0af48332637a67ba07d9eddf7c62ef8707f63
Felicia Schenkelberg <115110764+fschenkelberg@users.noreply.github.com>
2026-07-26 10:55:28 -0400

    ci(workflow): add comprehensive CI/CD pipeline (#4)

--- backend/src/test/java/com/mycompany/pm/PmApplicationTests.java
+++ backend/src/test/java/com/mycompany/pm/PmApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mycompany.project_management;
+package com.mycompany.pm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectManagementApplicationTests {
+class PmApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
